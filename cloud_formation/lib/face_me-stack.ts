67fee974c62f66e017ef224dd1226fc2828233ff
Shardul Vaidya <sherlock@thinkpad-ubuntu>
2022-03-11 13:20:23 -0500

    Adding CDK code

---
+++ cloud_formation/lib/face_me-stack.ts
@@ -0,0 +1,37 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as path from 'path';
+import * as cdk from "aws-cdk-lib/core";
+import * as iot from "aws-cdk-lib/aws-iot";
+import * as iotEvents from "aws-cdk-lib/aws-iotevents";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class FaceMeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const edge_device = new iot.CfnThing(this, 'FaceMeThing', {
+      thingName: 'FaceMe-Jetson'
+    });
+
+    const fn = new lambda.Function(this, 'FaceMeLambdaFunction', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'main-face_me.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
+    });
+
+    const iam_role = fn.role;
+
+    fn.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambdaDynamoDBExecutionRole'));
+    fn.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSIoTFullAccess'))
+    // fn.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(''))
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CloudFormationQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
