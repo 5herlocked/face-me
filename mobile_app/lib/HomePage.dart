e0c3dd763156325ab294c08da6bbc0703ae2f13c
Shardul Vaidya <cam.v737@gmail.com>
2022-03-20 16:42:41 -0500

    Added in Video player Deps, setup basic scaffolding, need to setup kinesis when I get home

--- mobile_app/lib/HomePage.dart
+++ mobile_app/lib/HomePage.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +10,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SingleChildScrollView(),
+        ],
+      ),
+    );
   }
 }
--- mobile_app/lib/SignInPage.dart
+++ mobile_app/lib/SignInPage.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class SignInPage extends StatefulWidget {
-    SignInPage({Key? key}) : super(key: key);
+  SignInPage({Key? key}) : super(key: key);
 
-    @override
-    _SignInPageState createState() => _SignInPageState();
+  @override
+  _SignInPageState createState() => _SignInPageState();
 }
 
 class _SignInPageState extends State<SignInPage> {
-    String userName = "";
-    String password = "";
+  String userName = "";
+  String password = "";
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        TextInput(),
-                        TextInput(),
-                        Button(),
-                        Button(),
-                    ]
-                )
-            )
-        )
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: "User Name",
+            ),
+            onChanged: (value) {
+              userName = value;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Password",
+            ),
+            onChanged: (value) {
+              password = value;
+            },
+          ),
+          MaterialButton(
+            child: Text("Sign In"),
+            onPressed: () {
+              Navigator.pop(context, {
+                "userName": userName,
+                "password": password,
+              });
+            },
+          ),
+        ])));
+  }
 }
