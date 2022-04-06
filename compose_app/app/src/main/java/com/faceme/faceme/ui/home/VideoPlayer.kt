5694fba6fb91b0681d200a266c1c87a7991408a2
Shardul Vaidya <cam.v737@gmail.com>
2022-04-05 22:48:28 -0400

    finished basic setup of video player

--- compose_app/app/src/main/java/com/faceme/faceme/ui/home/VideoPlayer.kt
+++ compose_app/app/src/main/java/com/faceme/faceme/ui/home/VideoPlayer.kt
@@ -3,16 +3,19 @@ package com.faceme.faceme.ui.home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import com.google.android.exoplayer2.SimpleExoPlayer
-import javax.sql.DataSource
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
 @Composable
 fun VideoPlayer() {
     val context = LocalContext.current
 
     val exoPlayer = remember(context) {
-        SimpleExoPlayer.Builder(context).build().apply {
-            val dataSourceFactory: DataSource.
+        ExoPlayer.Builder(context).build().also { exoPlayer ->
+            /* TODO: Finish implementing this */
+            val mediaItem = MediaItem.fromUri("")
+
+            exoPlayer.setMediaItem(mediaItem)
         }
     }
 }
