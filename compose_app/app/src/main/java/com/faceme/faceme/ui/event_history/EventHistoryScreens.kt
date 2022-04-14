481209c376123c0829805536cc1aa41c0c421410
Shardul Vaidya <cam.v737@gmail.com>
2022-04-13 23:35:24 -0400

    Event History Updates?

--- compose_app/app/src/main/java/com/faceme/faceme/ui/event_history/EventHistoryScreens.kt
+++ compose_app/app/src/main/java/com/faceme/faceme/ui/event_history/EventHistoryScreens.kt
@@ -3,6 +3,7 @@ package com.faceme.faceme.ui.event_history
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -14,7 +15,34 @@ fun EventHistoryWithEventDetailsScreen(
     openDrawer: () -> Unit,
     scaffoldState: ScaffoldState,
     eventHistoryLazyListState: LazyListState,
-    eventDetailLazyListStates: Map<String, LazyListState>
+    eventDetailLazyListStates: Map<String, LazyListState>,
+    modifier: Modifier = Modifier
+) {
+    EventHistoryScreenWithList(
+        uiState = uiState,
+        showTopAppBar = showTopAppBar,
+        onInteractWithHistory = onInteractWithHistory,
+        onInteractWithEventDetails = onInteractWithEventDetails,
+        openDrawer = openDrawer,
+        scaffoldState = scaffoldState,
+    ) {
+        hasEventsUiState, contentModifier ->
+
+    }
+}
+
+@Composable
+fun EventHistoryScreenWithList(
+    uiState: EventHistoryUiState,
+    showTopAppBar: Boolean,
+    onInteractWithHistory: () -> Unit,
+    onInteractWithEventDetails: (String) -> Unit,
+    openDrawer: () -> Unit,
+    scaffoldState: ScaffoldState,
+    hasEventsContent: @Composable (
+        uiState: EventHistoryUiState.HasEvents,
+        modifier: Modifier
+    ) -> Unit
 ) {
 
 }
@@ -29,7 +57,11 @@ fun EventHistoryScreen(
     scaffoldState: ScaffoldState,
     eventHistoryLazyListState: LazyListState
 ) {
+    EventHistoryScreenWithList(
 
+    ) { hasEventsUiState, contentModifier ->
+
+    }
 }
 
 @Preview("Event History Screen with Event Details")
